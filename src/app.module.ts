16d611ed02aa0f8293fa194a9102b60f165061f4
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-11 15:44:54 -0400

    setup: setup database joi schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '@app/common/database/database.module';
+import { ConfigModule } from '@app/common/config/config.module'; // Adjust the import path as necessary
 
 @Module({
+  imports: [ConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
-  imports: [DatabaseModule],
 })
 export class AppModule { }
