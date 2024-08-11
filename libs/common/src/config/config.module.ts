04ff4e8f162176049ba7723b7e15e231fe8aaea0
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-11 15:02:33 -0400

    setup: create a wrapper for the NestConfigModule

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
-@Module({})
-export class ConfigModule {}
+@Module({
+    imports: [NestConfigModule.forRoot()] // we are doing this, because we want to change configs in one place. So we are creating a wrapper here.
+})
+export class ConfigModule { }
