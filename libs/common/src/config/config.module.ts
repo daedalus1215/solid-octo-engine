491584da47306d04095a07de95980f78139081ea
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-11 15:13:06 -0400

    setup: Add more pieces to config.module and database.module

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { ConfigService } from 'aws-sdk';
 
 @Module({
-    imports: [NestConfigModule.forRoot()] // we are doing this, because we want to change configs in one place. So we are creating a wrapper here.
+    imports: [NestConfigModule.forRoot()], // we are doing this, because we want to change configs in one place. So we are creating a wrapper here.
+    providers: [ConfigService],
+    exports: [ConfigService]
 })
 export class ConfigModule { }
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/sleepr')]
+    imports: [MongooseModule.forRootAsync({ //nestjs provides us this in order to load up these configs 
+        imports: [ConfigModule], // ConfigModule is exporting the ConfigService for us.
+        useFactory: (configService:ConfigService) => ({ // tell nest how to create the mongoose
+            uri: configService.get('MONGODB_URI'),
+        }),
+        inject: [ConfigService] // tell nestJS of the providers we are injecting
+    })]
 })
 export class DatabaseModule { }
