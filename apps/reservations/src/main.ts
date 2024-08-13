3074f41012e3f7ebaa4afc8f2349be66f004104a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-12 20:42:14 -0400

    validation-and-logging: add comment as to what whitelist does

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,8 +5,13 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-  app.useLogger(app.get(Logger))
+  // When whitelist is set to true, the ValidationPipe will only include 
+  // properties that are present in the request body or query parameters 
+  // if they match with the model properties. It effectively "whitelists" 
+  // the properties, meaning it only accepts the expected ones and discards 
+  // any additional ones
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
