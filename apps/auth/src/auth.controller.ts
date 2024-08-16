358f229b2b76e801d878710d3e5171c090a1c94d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-15 20:46:00 -0400

    passport: add route to auth request handlers

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
-@Controller()
+@Controller('login')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @UseGuards(LocalAuthGuard)
+  async login() {
+
+  }
 
   @Get()
   getHello(): string {
