a058e46ffce6eaeb1265d8e515a7d5f59c0453ab
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-15 21:05:16 -0400

    Passport: fill in login RH on AuthController with AuthService.login()

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { UserDocument } from './users/user.schema';
+import { Response } from 'express';
 
 @Controller('login')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @Post()
   @UseGuards(LocalAuthGuard)
-  async login() {
-
-  }
-
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @Post()
+  async login(@CurrentUser() user: UserDocument,
+    @Res({ passthrough: true }) response: Response) {
+    await this.authService.login(user, response);
+    response.send(user);
   }
 }
