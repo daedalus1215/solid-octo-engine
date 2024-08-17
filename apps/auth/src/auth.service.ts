c96107994189c0df47075903305f1ad2aa29ad83
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2024-08-17 14:07:44 -0400

    passport-jwt-strategy: utilize new TokenPayload interface

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -3,6 +3,7 @@ import { UserDocument } from './users/user.schema';
 import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
@@ -10,14 +11,14 @@ export class AuthService {
   constructor(private readonly configService: ConfigService, private readonly jwtService: JwtService) { }
 
   async login(userInfo: UserDocument, response: Response) {
-    const tokenPayload = {
+    const tokenPayload: TokenPayload = {
       userId: userInfo._id.toHexString(),
     }
 
     const expires = new Date();
     expires.setSeconds(expires.getSeconds()) + this.configService.get('JWT_EXPIRATION')
     const token = this.jwtService.sign(tokenPayload);
-    
+
     response.cookie('Authentication', token, {
       httpOnly: true,
       expires
